40d8821560b22091e3e1efbc54a5417a32f1764f
Nightxade <matthewcaieatsrice@gmail.com>
2023-07-12 19:57:31 -0400

    Added preprocessing -- to be completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from NeuralNetworkNet import NeuralNetworkNet
 from DNNDataLoader import DNNDataLoader
+import matplotlib.pyplot as plt
 
 #import data
 data = np.loadtxt(fname='breast_cancer_data/wdbc.data', dtype=str, delimiter=',')
@@ -25,18 +26,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 numEpochs = 30
 batch_size = 32
 
-for epoch in range(numEpochs):
-	print(f"epoch {epoch}")
-	for i, (xBatch, yBatch) in enumerate(trainDataLoader):
-		yPredToTrain = model(xBatch)
-
-		#print(yPredToTrain, yBatch)
-
-		loss = loss_fn(yPredToTrain.squeeze(), yBatch.squeeze())
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-
+# create testing data
 testInput: torch.Tensor = torch.empty(0)
 testOutput: torch.Tensor = torch.empty(0)
 # testOutput = testOutput.unsqueeze(0)
@@ -50,8 +40,41 @@ for batchInput, batchOutput in testDataLoader:
 
 testInput = testInput.reshape(testInput.size()[0] // 29, 29)
 
-#predict values with testing data input
-yPred = model(testInput)
+# simulate epochs
+testAccuracies = []
+for epoch in range(numEpochs):
+	print(f"epoch {epoch + 1}")
+	for i, (xBatch, yBatch) in enumerate(trainDataLoader):
+		yPredToTrain = model(xBatch)
+
+		#print(yPredToTrain, yBatch)
+
+		loss = loss_fn(yPredToTrain.squeeze(), yBatch.squeeze())
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+	# Testing
+	# predict values with testing data input
+	yPred = model(testInput)
+
+	threshold = 0.5  # Adjust the threshold based on your requirements
+
+	# Convert predicted probabilities to binary predictions
+	binary_predictions = (yPred >= threshold).int()
+
+	# Convert binary_predictions to float
+	binary_predictions = binary_predictions.float().view(-1)
+	testOutput = testOutput.view(-1)
+
+	# print(binary_predictions)
+	# print(testOutput)
+
+	# Calculate accuracy
+	accuracy = (binary_predictions == testOutput).float().mean()
+	testAccuracies.append(accuracy.item() * 100)
+
+# print(testAccuracies)
 
 # #Calculate Mean Absolute Error (MAE)
 # mae = torch.abs(yPred - testOutput).mean()
@@ -64,6 +87,8 @@ yPred = model(testInput)
 # print(f"MAE {mae}")
 # print(f"r2 {r2}")
 
+# Final test accuracy
+
 threshold = 0.5  # Adjust the threshold based on your requirements
 
 # Convert predicted probabilities to binary predictions
@@ -73,10 +98,18 @@ binary_predictions = (yPred >= threshold).int()
 binary_predictions = binary_predictions.float().view(-1)
 testOutput = testOutput.view(-1)
 
-print(binary_predictions)
-print(testOutput)
+# print(binary_predictions)
+# print(testOutput)
 
 # Calculate accuracy
 accuracy = (binary_predictions == testOutput).float().mean()
 
-print(f"Accuracy: {accuracy.item()}")
+print(f"Accuracy: {accuracy.item() * 100}")
+
+# Plot test accuracy
+plt.plot(np.arange(1, numEpochs+1), testAccuracies)
+plt.title("Test Accuracy over Epochs")
+plt.xlabel("Epochs")
+plt.ylabel("Test Accuracy")
+plt.ylim(50, 100)
+plt.show()
